70e8fd51acc3f2f6bac206b6c8ac3ac0662c4bd0
Milton dos Santos Guilherme Neto <milton.neto@gmail.com>
2022-09-07 16:18:17 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dsmeta.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmeta.dsmeta.config.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+	List<Sale> findAll();
+
+}
+
+
